16395530578f829dba5fdba1ad05067e96af9cc1
MohitNarula91 <mohit.narula@nagarro.com>
2020-01-26 22:40:30 +0530

    Changes with Zuul working

--- NAGP-Microservices-Session/prices/src/main/java/org/selflearning/msa/prices/controller/PriceController.java
+++ NAGP-Microservices-Session/prices/src/main/java/org/selflearning/msa/prices/controller/PriceController.java
@@ -3,6 +3,7 @@ package org.selflearning.msa.prices.controller;
 import javax.annotation.Resource;
 
 import org.selflearning.msa.prices.services.PriceCalculationService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/price")
 public class PriceController {
 
+	@Value("${server.port}")
+	private int port;
+
 	@Resource
 	PriceCalculationService priceCalculationService;
 
@@ -19,9 +23,8 @@ public class PriceController {
 	Double getProductPrice(@RequestParam(name = "goldWeight") Double goldWeight,
 			@RequestParam(name = "goldPurity") String goldPurity,
 			@RequestParam(name = "pearlsWeight") Double pearlsWeight) {
-		return priceCalculationService.calculateProductPrice(goldWeight,goldPurity,pearlsWeight);
+		System.out.println("Working from port " + port + " of price service");
+		return priceCalculationService.calculateProductPrice(goldWeight, goldPurity, pearlsWeight);
 	}
-	
-	
 
 }
--- NAGP-Microservices-Session/products/src/main/java/org/selflearning/msa/products/controllers/ProductsController.java
+++ NAGP-Microservices-Session/products/src/main/java/org/selflearning/msa/products/controllers/ProductsController.java
@@ -4,20 +4,25 @@ import javax.annotation.Resource;
 
 import org.selflearning.msa.products.entities.Product;
 import org.selflearning.msa.products.services.ProductsDetailService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/products")
+@RequestMapping(value = "/products")
 public class ProductsController {
+
+	@Value("${server.port}")
+	private int port;
 
 	@Resource
 	ProductsDetailService productsDetailService;
-	
-	@GetMapping(value="/{designNumber}")
-	Product getProductDetails(@PathVariable(name="designNumber") String designNumber){
+
+	@GetMapping(value = "/{designNumber}")
+	Product getProductDetails(@PathVariable(name = "designNumber") String designNumber) {
+		System.out.println("Working from port " + port + " of product service");
 		return productsDetailService.getProductByDesignNumber(designNumber);
 	}
 }
